5225f36978014ec374ab3feb761c05b0a519515e
tishetiho <moteoAZ@yandex.ru>
2026-05-10 08:06:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -259,11 +259,11 @@ invoice = await crypto.create_invoice(
     currency_type='fiat',
     accepted_assets='USDT,TON' # Список валют, которые ты готов принять
 )
-        await callback.message.answer(
-            f"💳 Оплатите товар **{item[0]}**\n"
-            f"Сумма: {item[1]} ₽\n\n"
-            f"После оплаты бот автоматически откроет чат с продавцом.",
-            reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+    await callback.message.answer(
+        f"💳 Оплатите товар **{item[0]}**\n"
+        f"Сумма: {item[1]} ₽\n\n"
+        f"После оплаты бот автоматически откроет чат с продавцом.",
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Оплатить", url=invoice.bot_invoice_url)],
         [InlineKeyboardButton(text="Проверить оплату", callback_data=f"check_{invoice.invoice_id}_{item_id}")]
         ]))
